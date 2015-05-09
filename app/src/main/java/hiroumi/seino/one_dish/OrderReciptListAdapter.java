a037c92fa6728e0d2309dacc082861d58c01ed04
Mac User <laser_yellow@cnc.jp>
2015-05-10 00:41:28 +0900

    no message

--- app/src/main/java/hiroumi/seino/one_dish/OrderReciptListAdapter.java
+++ app/src/main/java/hiroumi/seino/one_dish/OrderReciptListAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -29,9 +31,18 @@ public class OrderReciptListAdapter extends ArrayAdapter {
 
         if( convertView == null ){
 
-
+            convertView = inflater.inflate( R.layout.order_recipt_grid_item , null );
 
         }
+
+        ImageView image = ( ImageView ) convertView.findViewById( R.id.order_recipt_grid_item_image );
+        View view = convertView.findViewById(R.id.order_recipt_grid_item_text_layout);
+        TextView title = ( TextView ) view.findViewById( R.id.order_recipt_grid_item_text_title );
+        View inner = view.findViewById( R.id.order_recipt_grid_item_text_inner );
+        TextView time = ( TextView ) inner.findViewById( R.id.order_recipt_grid_item_text_time );
+        TextView cal = ( TextView ) inner.findViewById( R.id.order_recipt_grid_item_text_cal );
+
+
 
         return convertView;
     }
--- app/src/main/java/hiroumi/seino/one_dish/OrderReciptListFragment.java
+++ app/src/main/java/hiroumi/seino/one_dish/OrderReciptListFragment.java
@@ -1,23 +1,29 @@
 package hiroumi.seino.one_dish;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
 
-public class OrderReciptListFragment extends ListFragment {
-
-    public static OrderReciptListFragment newInstance(String param1, String param2) {
-        OrderReciptListFragment fragment = new OrderReciptListFragment();
-        Bundle args = new Bundle();
-        return fragment;
-    }
+public class OrderReciptListFragment extends Fragment {
 
     public OrderReciptListFragment() {
     }
 
     @Override
-    public void onActivityCreated( Bundle saved ){
-        super.onActivityCreated( saved );
+    public View onCreateView( LayoutInflater inflater , ViewGroup group , Bundle savedInstanceState ){
 
+        GridView view = new GridView( getActivity().getBaseContext() );
+        //Todo:OrderReciptListAdapter adapter = new OrderReciptListAdapter( getActivity().getBaseContext() ,  );
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated( View view , Bundle savedInstanceState ){
+        super.onViewCreated( view , savedInstanceState );
 
 
     }
