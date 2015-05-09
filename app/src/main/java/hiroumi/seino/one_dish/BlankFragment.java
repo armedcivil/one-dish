56e0c3bdc3ed13306705b5d29833d2ba7d2a5e5c
Mac User <laser_yellow@cnc.jp>
2015-05-09 15:36:26 +0900

    create Order List fragment

---
+++ app/src/main/java/hiroumi/seino/one_dish/BlankFragment.java
@@ -0,0 +1,45 @@
+package hiroumi.seino.one_dish;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment extends Fragment {
+
+    public static BlankFragment newInstance(String param1, String param2) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated( View view ){
+
+
+
+    }
+
+}
