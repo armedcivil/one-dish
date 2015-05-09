70f7e94e6963a42d56e432379e18ddab6ead5a15
Mac User <laser_yellow@cnc.jp>
2015-05-09 17:53:14 +0900

    no message

---
+++ app/src/main/java/hiroumi/seino/one_dish/OrderActivity.java
@@ -0,0 +1,20 @@
+package hiroumi.seino.one_dish;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class OrderActivity extends ActionBarActivity {
+    private static final String TAG = OrderActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate( Bundle savedInstanceState){
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.order_activity );
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+}
